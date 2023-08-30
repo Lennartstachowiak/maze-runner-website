24f4eab29278dd39a04f1bb45b3b9408b344e7e2
Lennart <lennart.stachowiak@code.berlin>
2023-08-30 13:45:10 +0200

    change position of dotenv

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
-import dotenv from "dotenv";
-dotenv.config();
 
 export default function Document() {
   return (
